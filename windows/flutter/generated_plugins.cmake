66ee651650746bffcd93ac2d33382911ab32c6ee
arunteja30 <arunteja30@users.noreply.github.com>
2026-09-03 15:50:51 +0530

    Configure Flutter Web with FirebaseOptions and Google Maps JavaScript API script

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -4,6 +4,7 @@
 
 list(APPEND FLUTTER_PLUGIN_LIST
   battery_plus
+  file_selector_windows
   firebase_auth
   firebase_core
   geolocator_windows
@@ -12,6 +13,7 @@ list(APPEND FLUTTER_PLUGIN_LIST
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
+  jni
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)
